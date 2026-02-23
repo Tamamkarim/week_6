94faeb9308597b765dd2cc96c4422533d26a5c6b
mattpe <mattpe@metropolia.fi>
2026-02-23 11:07:20 +0200

    dev environment ready

--- App.tsx
+++ App.tsx
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, Platform} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
-export default function App() {
+const App = () => {
+  console.log('First app version working!!')
   return (
-    <View style={styles.container}>
+    <SafeAreaProvider style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaProvider>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    //justifyContent: 'center',
+    // quick safearea fix for status bar
+    paddingTop: Platform.OS === 'android' ? 20 : 0,
   },
 });
+
+export default App;
