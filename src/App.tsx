b4a5c267c1049992651588f031c5d68a3231b1fd
mattpe <mattpe@metropolia.fi>
2026-02-24 13:57:20 +0200

    fix safeareaview

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, Platform} from 'react-native';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 
 const App = () => {
-  console.log('First app version working!!')
+  //console.log('First app version working!!');
   return (
-    <SafeAreaProvider style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 };
@@ -18,8 +20,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     //justifyContent: 'center',
-    // quick safearea fix for status bar
-    paddingTop: Platform.OS === 'android' ? 20 : 0,
   },
 });
 
