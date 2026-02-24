45ce434ac873f4f1fc164cdd832e45cc3f84543e
mattpe <mattpe@metropolia.fi>
2026-02-24 16:18:22 +0200

    update MediaListItem layout and styles

--- src/components/MediaListItem.tsx
+++ src/components/MediaListItem.tsx
@@ -10,16 +10,23 @@ const MediaListItem = ({item}: {item: MediaItemWithOwner}) => {
         console.log('touched ' + item.title);
       }}
     >
-      <Text>MediaListItem {item.title}</Text>
+      <Text style={styles.title}>{item.title}</Text>
       <Image style={styles.image} source={{uri: item.thumbnail}} />
       <Text>{item.description}</Text>
+      <Text>
+        Created at: {new Date(item.created_at).toLocaleString('fi-FI')}
+      </Text>
+      <Text>Filesize: {(item.filesize / 1024 / 1024).toFixed(2)} MB</Text>
+      <Text>Mime-type: {item.media_type}</Text>
+      <Text>Owner: {item.username}</Text>
       {/* TODO = HOMEWORK: add all other media data and style your list & listitems */}
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {paddingTop: 20},
+  container: {marginTop: 5, marginBottom: 5, backgroundColor: '#f5c668'},
+  title: {fontSize: 20},
   image: {height: 300},
 });
 
